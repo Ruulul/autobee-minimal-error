2f91cd0094e90be95ad9966903744b7f3c3ac2d8
Ruulul <vamjunior01@gmail.com>
2023-05-03 18:19:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import * as SDK from 'hyper-sdk'
+import Autobase from 'autobase'
+import { DB } from 'hyperdeebee'
+import Autodeebee from 'hyperdeebee/autodeebee.js'
+import goodbye from 'graceful-goodbye'
+
+const sdk = await SDK.create({
+    storage: false,
+})
+goodbye(_ => sdk.close())
+
+const IOCores = await sdk.namespace('example')
+const localInput = IOCores.get({ name: 'local-input' })
+const localOutput = IOCores.get({ name: 'local-output' })
+const autobase = new Autobase({ localInput, localOutput })
+const bee = new Autodeebee(autobase, { keyEncoding: 'binary', valueEncoding: 'binary' })
+
+const db = new DB(bee)
+
+const events = db.collection('events')
+await events.createIndex(['kind', 'created_at', 'pubkey', 'id'])
